5658c783e977384a2628a1c9118565d643b1500d
Eugene Cheah <eugeneqin@gmail.com>
2022-07-21 15:01:41 +0000

    Enable search + WIP logo support

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,9 +7,23 @@
 //
 
 const { defaultTheme } = require('@vuepress/theme-default')
+const { searchPlugin } = require('@vuepress/plugin-search')
 
 module.exports = {
 	theme: defaultTheme({
+		// Logo support
+		logo: "/static/logo.png",
+
+		// Support deep nesting of subpage "table-of-content"
+		sidebarDepth:10,
+
+		// Load the sidebar menu, from summary.md
 		sidebar: require("./summary-to-sidebar").default
-	})
+	}),
+
+	plugins: [
+		searchPlugin({
+			maxSuggestions: 10
+		}),
+	]
 }
